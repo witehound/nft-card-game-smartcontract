f27a47e9436f33a4ebeb8e0d3a480d89fcdae799
witehound <netvid00@gmail.com>
2022-12-31 15:11:32 +0100

    setup files for avaxgod smart contract andd ddeploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,6 @@ import "@nomiclabs/hardhat-ethers";
 
 dotenv.config();
 
-//* Notes for deploying the smart contract on your own subnet
-//* More info on subnets: https://docs.avax.network/subnets
-//* Why deploy on a subnet: https://docs.avax.network/subnets/when-to-use-subnet-vs-c-chain
-//* How to deploy on a subnet: https://docs.avax.network/subnets/create-a-local-subnet
-//* Transactions on the C-Chain might take 2-10 seconds -> the ones on the subnet will be much faster
-//* On C-Chain we're relaying on the Avax token to confirm transactions -> on the subnet we can create our own token
-//* You are in complete control over the network and it's inner workings
-
 export default {
   solidity: {
     version: "0.8.16",
@@ -29,11 +21,5 @@ export default {
       chainId: 43113,
       accounts: [process.env.PRIVATE_KEY],
     },
-    // subnet: {
-    //   url: process.env.NODE_URL,
-    //   chainId: Number(process.env.CHAIN_ID),
-    //   gasPrice: 'auto',
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
   },
 };
